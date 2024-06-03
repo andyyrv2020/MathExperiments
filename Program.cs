3773d9dbb349b56c1cfbea067865286870bdbc63
PC-23 <it.pgee.burgas@gmail.com>
2024-06-03 09:25:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Please state your number:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the type of your system:");
+            int system = int.Parse(Console.ReadLine());
+            string answer = "";
+
+            while (x != 0)
+            {
+                answer = answer + x % system;
+                x = x / system;
+            }
+            Console.WriteLine(answer.Reverse().ToArray());
+        }
+    }
+
